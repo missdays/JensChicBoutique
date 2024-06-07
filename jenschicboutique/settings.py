3784b07774d95d1757de274e6f7e55720e6626ab
missdays <dyeneffer13@hotmail.com>
2024-06-07 09:59:15 +0100

    add secret key

--- jenschicboutique/settings.py
+++ jenschicboutique/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3v*n$v7j8(5yi&&f$1kpf#iz5jhj(0etp^f5#e%7f6ui^_w#+q'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'jenschicboutique-cc2328e16ed2.herokuapp.com']
 
